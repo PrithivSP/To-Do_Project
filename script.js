5f39517cad12f5d934955dd6eb1b740fe3d49ee4
Prithiv SP <prithivspn@gmail.com>
2021-08-20 22:08:04 +0530

    Can add ToDos

---
+++ script.js
@@ -0,0 +1,43 @@
+// Selectors
+
+const toDoInput = document.querySelector('.todo-input');
+const toDoBtn = document.querySelector('.todo-add-btn');
+const toDoList = document.querySelector('.todo-list');
+
+// EventListener
+toDoBtn.addEventListener('click', addToDo);
+
+// Functions
+
+function addToDo(event) {
+    
+    event.preventDefault();
+
+    //! ToDo Div
+   const toDoDiv = document.createElement('div');
+   toDoDiv.classList.add('toDos');
+
+   //! Create Element
+   const newToDo = document.createElement('li');
+   newToDo.classList.add('todo-item');
+   newToDo.innerText = toDoInput.value ;
+   toDoDiv.appendChild(newToDo);
+
+   //! Completed Button
+   const CompletedBtn = document.createElement('button');
+   CompletedBtn.innerHTML = '<i class="fas fa-check"></i>';
+   CompletedBtn.classList.add('complete-Btn');
+   toDoDiv.appendChild(CompletedBtn);
+
+   //! Delete Button
+   const DeleteBtn = document.createElement('button');
+   DeleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
+   DeleteBtn.classList.add('Delete-Btn');
+   toDoDiv.appendChild(DeleteBtn);
+
+   //! Append to List container
+   toDoList.appendChild(toDoDiv);
+
+   //! clear ToDo Input
+   toDoInput.value= "";
+}
